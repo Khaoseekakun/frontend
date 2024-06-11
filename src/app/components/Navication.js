1cadef09279d0029f1226c7c3c095ae884a44faf
Khao Seekakun <emitstudio13829@gmail.com>
2024-06-11 14:38:40 +0700

    update navication bar

--- src/app/components/Navication.js
+++ src/app/components/Navication.js
@@ -2,11 +2,35 @@ import Link from "next/link";
 
 export default function NavicationBar() {
     return (
-        <center>
-            <Link className="btn btn-primary mx-2 my-3" href="/">หน้าแรก</Link>
-            <Link className="btn btn-secondary mx-2 my-3" href="/about">เกี่ยวกับเรา</Link>
-            <Link className="btn btn-success mx-2 my-3" href="/service">บริการของเรา</Link>
-            <Link className="btn btn-danger mx-2 my-3" href="/contact">ติดต่อเรา</Link>
-        </center>
+        <>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link className="navbar-brand" href="#">Sinsamth</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" href="/">หน้าแรก</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" href="/about">เกี่ยวกับเรา</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" href="/service">บริการของเรา</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" aria-current="page" href="/contact">ติดต่อเรา</Link>
+                            </li>
+                        </ul>
+                        <form className="d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                            <button className="btn btn-outline-success" type="submit">Search</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
+        </>
     )
 }
